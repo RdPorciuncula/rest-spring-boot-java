a3538691118c03b08a18f6b11463818cddb715fb
Rodrigo de Jesus Porciuncula <rjporciuncula@Mac-mini-de-Rodrigo.local>
2024-07-24 18:24:15 -0300

    Adicionando injeção de dependência e utilização dos verbos http

---
+++ Project_SpringBoot_Java/rest-spring-boot-java/src/main/java/br/com/java/controllers/PersonController.java
@@ -0,0 +1,57 @@
+package br.com.java.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.java.model.Person;
+import br.com.java.services.PersonService;
+
+@RestController
+@RequestMapping("/person")
+public class PersonController {
+	
+	@Autowired // Ocorre em tempo de execução
+	private PersonService personService;
+	
+	@GetMapping(produces=MediaType.APPLICATION_JSON_VALUE)
+	public List<Person> findAll() {
+		return personService.findAll();
+	}
+	
+	@GetMapping(value = "/{id}", produces=MediaType.APPLICATION_JSON_VALUE)
+	public Person findById(@PathVariable(value = "id") String id) throws Exception {
+		
+		return personService.findById(id);
+	}
+	
+	@PostMapping(
+			consumes = MediaType.APPLICATION_JSON_VALUE, 
+			produces = MediaType.APPLICATION_JSON_VALUE)
+	public Person create(@RequestBody Person person) throws Exception {
+		
+		return personService.create(person);
+	}
+	
+	@PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE, 
+				produces = MediaType.APPLICATION_JSON_VALUE)
+	public Person update(@RequestBody Person person) throws Exception {
+		
+		return personService.update(person);
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public void delete(@PathVariable(value = "id") String id) throws Exception {
+		
+		personService.delete(id);
+	}
+}
---
+++ Project_SpringBoot_Java/rest-spring-boot-java/src/main/java/br/com/java/services/PersonService.java
@@ -0,0 +1,74 @@
+package br.com.java.services;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.logging.Logger;
+
+import org.springframework.stereotype.Service;
+
+import br.com.java.model.Person;
+
+@Service // Cuida da instanciação
+public class PersonService {
+	
+	private final AtomicLong counter = new AtomicLong();
+	private Logger logger = Logger.getLogger(PersonService.class.getName());
+	
+	public List<Person> findAll() {
+		
+		logger.info("Finding All people!");
+		
+		List<Person> persons = new ArrayList<>();
+		for(int i = 0; i < 8; i++) {
+			Person person = mockPerson(i);
+			persons.add(person);
+		}
+		
+		return persons ;
+	}
+
+	public Person findById(String id) {
+		
+		logger.info("Finding one person!");
+		
+		Person person = new Person();
+		person.setId(counter.incrementAndGet());
+		person.setFirstName("Rodrigo");
+		person.setLastName("Porciuncula");
+		person.setAddress("Salvador - Bahia");
+		person.setGender("Male");
+		
+		return person;
+	}
+	
+	public Person create(Person person) {
+		
+		logger.info("Creating one person!");
+		
+		return person;
+	}
+	
+	public Person update(Person person) {
+		
+		logger.info("Updating one person!");
+		
+		return person;
+	}
+	
+	public void delete(String id) {
+		
+		logger.info("Deleting one person!");
+	}
+	
+	private Person mockPerson(int i) {
+		Person person = new Person();
+		person.setId(counter.incrementAndGet());
+		person.setFirstName("Person Name: " + i);
+		person.setLastName("Last Name: " + i);
+		person.setAddress("Address: " + i);
+		person.setGender("Male");
+		return person;
+	}
+}
+
