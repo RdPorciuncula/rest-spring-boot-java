29653339a46afc12eafc2085b1e42767461940f6
Rodrigo de Jesus Porciuncula <rjporciuncula@Mac-mini-de-Rodrigo.local>
2024-07-28 14:01:51 -0300

    Integrando API com Base de dados mySql

--- Project_SpringBoot_Java/rest-spring-boot-java/src/main/java/br/com/java/controllers/PersonController.java
+++ Project_SpringBoot_Java/rest-spring-boot-java/src/main/java/br/com/java/controllers/PersonController.java
@@ -29,7 +29,7 @@ public class PersonController {
 	}
 	
 	@GetMapping(value = "/{id}", produces=MediaType.APPLICATION_JSON_VALUE)
-	public Person findById(@PathVariable(value = "id") String id) throws Exception {
+	public Person findById(@PathVariable(value = "id") Long id) throws Exception {
 		
 		return personService.findById(id);
 	}
@@ -50,7 +50,7 @@ public class PersonController {
 	}
 	
 	@DeleteMapping(value = "/{id}")
-	public void delete(@PathVariable(value = "id") String id) throws Exception {
+	public void delete(@PathVariable(value = "id") Long id) throws Exception {
 		
 		personService.delete(id);
 	}
