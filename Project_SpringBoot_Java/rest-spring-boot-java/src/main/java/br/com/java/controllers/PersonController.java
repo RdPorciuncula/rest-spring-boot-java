41683bfec313ee45843c9392e16499813b1ed597
Rodrigo de Jesus Porciuncula <rjporciuncula@Mac-mini-de-Rodrigo.local>
2024-09-17 19:52:34 -0300

    Inserindo migrations com flyway

--- Project_SpringBoot_Java/rest-spring-boot-java/src/main/java/br/com/java/controllers/PersonController.java
+++ Project_SpringBoot_Java/rest-spring-boot-java/src/main/java/br/com/java/controllers/PersonController.java
@@ -19,7 +19,7 @@ import br.com.java.data.vo.v2.PersonVOV2;
 import br.com.java.services.PersonService;
 
 @RestController
-@RequestMapping("/person")
+@RequestMapping("/api/person/v1")
 public class PersonController {
 	
 	@Autowired // Ocorre em tempo de execução
